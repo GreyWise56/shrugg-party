8fe345ee4afad21e0c95ff067e248b77301d7935
GreyWise <eric@jamgroupstudio.com>
2025-07-12 08:29:09 -0500

    split forces

--- shruggbot-ui/src/components/ShruggBot.js
+++ shruggbot-ui/src/components/ShruggBot.js
@@ -162,7 +162,7 @@ const ShruggBot = () => {
     setResponse(null);
 
     try {
-      const res = await fetch('/api/shrugg', {
+      const res = await fetch(process.env.REACT_APP_API_URL, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: inputText, mode: mode, tones: tones }),
